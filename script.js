763b0550c6e5f9e71789051d6eb17066453fd650
Fabio Roncari <fabio.roncari@gmail.com>
2024-04-17 20:09:51 +0000

    Add lesson of javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Fabio"
+let cognome = "Roncari"
+let message = `Ciao ${nome} ${cognome}`
+document.getElementById("nome").innerHTML = nome
+document.getElementById("cognome").innerHTML = cognome
+console.log(message)
